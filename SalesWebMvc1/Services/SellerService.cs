d5db8616c5a1243c77929a21602b0542fc8711b1
Helio Mendes <helio.msj@outlook.com>
2021-12-27 19:23:04 -0300

    Simple Create form

--- SalesWebMvc1/Services/SellerService.cs
+++ SalesWebMvc1/Services/SellerService.cs
@@ -18,5 +18,10 @@ namespace SalesWebMvc1.Services
         {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
