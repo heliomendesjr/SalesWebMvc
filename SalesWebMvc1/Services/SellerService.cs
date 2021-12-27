bd89edcbc2b71f26dfc001ba7bad30ff32ec5e15
Helio Mendes <helio.msj@outlook.com>
2021-12-27 19:40:17 -0300

    Foreign Key not null (referential integrity)

--- SalesWebMvc1/Services/SellerService.cs
+++ SalesWebMvc1/Services/SellerService.cs
@@ -20,6 +20,7 @@ namespace SalesWebMvc1.Services
         }
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
